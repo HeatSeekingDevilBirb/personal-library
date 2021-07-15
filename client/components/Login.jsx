65adff84d76a2b4f7eef959ec0fc51a69fcb8bfd
cjamesb <cjberry1234@sbcglobal.net>
2021-07-15 13:15:34 -0700

    forms working for signin and signup it handles the form submit

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Grid, Paper, Avatar, TextField, FormControlLabel, Checkbox, Button, Typography, Link} from "@material-ui/core";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 
 
 const Login = ({handleChange}) => {
+   
     
     const paperStyle={padding:20, width:280, height:'70vh', margin:'0'}
     const fieldStyle={margin: '8px 0'}
 
     const avatarStyle={backgroundColor: 'green'}
     const buttonStyle={margin: "8px 0"}
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("username", username, 'password', password);
+
+    }
+    
 
 
 
@@ -22,9 +33,28 @@ const Login = ({handleChange}) => {
             <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
              <h2>Sign In</h2>
              </Grid>
-             <TextField id="outlined-basic" label="Username" variant="outlined" placeholder='Enter username' style={fieldStyle} fullWidth required />
-             <TextField id="outlined-basic" label="Password" variant="outlined" placeholder='Enter password' style={fieldStyle} type='password' fullWidth required />
-             <FormControlLabel
+             <form onSubmit={handleSubmit}>
+             <TextField
+             value={username} 
+             onInput={e=>setUsername(e.target.value)}
+             id="outlined-basic" 
+             label="Username" 
+             variant="outlined" 
+             placeholder='Enter username' 
+             style={fieldStyle} 
+             fullWidth required />
+
+             <TextField 
+             value={password} 
+             onInput={e=>setPassword(e.target.value)}
+             id="outlined-basic" 
+             label="Password" 
+             variant="outlined" 
+             placeholder='Enter password' 
+             style={fieldStyle} 
+             type='password' 
+             fullWidth required />
+             <FormControlLabel onSubmit
                 control={
                 <Checkbox
                
@@ -34,9 +64,11 @@ const Login = ({handleChange}) => {
                  }
                 label="Remember me"
                 />
+               
                 <Button type='submit' color='primary' fullWidth variant='contained' style={buttonStyle}>
                     Sign In
                 </Button>
+                </form>
                 <Typography>
                     <Link to='/signinup'>Forgot password ?</Link>
                 </Typography>
